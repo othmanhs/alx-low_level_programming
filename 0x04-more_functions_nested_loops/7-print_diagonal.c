e033fae7870110b3703ebbf735e785629fa72922
othman hs <atmanhs2001@gmail.com>
2023-02-24 05:18:51 -0800

    7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,21 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		for (postn = 1; postn <= n; ++postn)
-		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
-	}
+        int x, y;
+        if (n <= 0)
+        {
+        _putchar('\n');
+        }
+        else
+        {
+        for (x = 0; x < n; x++)
+        {
+        for (y = 0; y < x; y++)
+        {
+        _putchar(32);
+        }
+        _putchar(92);
+        _putchar('\n');
+        }
+        }
 }
